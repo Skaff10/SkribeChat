2a53324dffd563a4c248b288fe984eab1ddb51c5
Skaff10 <str.skaff@gmail.com>
2025-07-23 00:13:04 +0600

    F

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true, // required for external access (Cloudflare Tunnel)
+    port: 5173, // or your preferred port
+    allowedHosts: [".trycloudflare.com"], // allow all trycloudflare subdomains
+  },
 });
